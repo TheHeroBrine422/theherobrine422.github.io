72c543d29ca53414edf6289e681d86b4bfd7a959
Caleb Jones <caleb@calebgj.io>
2018-11-29 16:56:24 -0600

    update data 11/29/18 16:56:24 CST

--- nodejsUpdateLeaderboardJSON/update.sh
+++ nodejsUpdateLeaderboardJSON/update.sh
@@ -3,14 +3,14 @@
 SIZE=2 # smaller = bigger
 clear
 
-COUNTER=$(cat readableData.json | underscore select '.key5participants' --outfmt text) # set counter to the first data page it needs to pull (first page that isnt just 5s)
+COUNTER=$(cat readableData.json | underscore select '.key5participants' | tr '\n' ' ' | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | sed 's/ /\n/g') # set counter to the first data page it needs to pull (first page that isnt just 5s)
 COUNTER=$(($COUNTER/100))
 TOTAL=$(echo $COUNTER | awk '{printf("%d\n",$0+=$0<0?-0.5:0.5)}')
 COUNTER=$(($COUNTER-1))
 
 node updateLeaderboard.js $COUNTER # get new total
 
-TOTAL=$(cat leaderboard.json | underscore select '.total' --outfmt text) # partical creadit https://stackoverflow.com/questions/17883661/how-to-extract-numbers-from-a-string
+TOTAL=$(cat leaderboard.json | underscore select '.total' | tr '\n' ' ' | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | sed 's/ /\n/g') # partical creadit https://stackoverflow.com/questions/17883661/how-to-extract-numbers-from-a-string
 TOTAL=$(($TOTAL/100))
 TOTAL=$(echo $TOTAL | awk '{printf("%d\n",$0+=$0<0?-0.5:0.5)}')
 TOTAL=$(($TOTAL+1)) # pull total
@@ -43,11 +43,9 @@ done
 
 node convertIntoReadableData.js # convert the data into a readable form for the website
 
-
-
 TIME=$(date +"%D %T") # update the data on the webserver (github)
-TIMEWRITE=$(cat readableData.json | underscore extend '{time: $TIME}')
+TIMEWRITE=$(cat readableData.json | underscore extend '{time: "$TIME"}')
 echo $TIMEWRITE > readableData.json
 git add *
 git commit -m "update data $TIME CST"
-git push
+#git push
