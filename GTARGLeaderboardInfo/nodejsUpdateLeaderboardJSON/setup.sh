b282392f68104520dad3a7f0cf4a99bd5501da03
Caleb Jones <caleb@calebgj.io>
2019-01-19 18:20:34 -0600

    gtarg fix setup and index

--- GTARGLeaderboardInfo/nodejsUpdateLeaderboardJSON/setup.sh
+++ GTARGLeaderboardInfo/nodejsUpdateLeaderboardJSON/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-echo {"participants":100000,"key9participants":0,"key8participants":0,"key6participants":0,"key6participants":0,"key5participants":0,"key4participants":0"key3participants":0,"key2participants":0,"key1participants":0,"ranking50p":"0","ranking25p":"0","ranking10p":"0","ranking5p":"0","ranking1p":"0","time":"01/16/19 10:30:09"} >> readableData.json
-echo {"total":95300,"results":[null]} >> leaderboard.json
+rm -rf leaderboard.json readableData.json
+
+echo \{\"participants\":100000,\"key9participants\":0,\"key8participants\":0,\"key6participants\":0,\"key6participants\":0,\"key5participants\":0,\"key4participants\":0,\"key3participants\":0,\"key2participants\":0,\"key1participants\":0,\"ranking50p\":\"0\",\"ranking25p\":\"0\",\"ranking10p\":\"0\",\"ranking5p\":\"0\",\"ranking1p\":\"0\",\"time\":\"01/16/19 10:30:09\"\} >> readableData.json
+echo \{\"total\":95300,\"results\":[null]\} >> leaderboard.json
