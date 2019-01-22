7ae6b396139d9bab96fc9b9348a1339687410468
Caleb Jones <caleb@calebgj.io>
2019-01-22 13:35:44 -0600

    gtarg make eta faster/shorter

--- GTARGLeaderboardInfo/nodejsUpdateLeaderboardJSON/eta.js
+++ GTARGLeaderboardInfo/nodejsUpdateLeaderboardJSON/eta.js
@@ -1,15 +1,5 @@
-var count
-var total
-var currentTime
-var startTime
-
-count = process.argv[2]
-total = process.argv[3]
-currentTime = process.argv[4]
-startTime = process.argv[5]
-
-var percent = 100*count/total
-var eta1 = currentTime - startTime
+var percent = 100*process.argv[2]/process.argv[3]
+var eta1 = process.argv[4] - process.argv[5]
 var eta2 = 100/percent
 var eta = eta1*eta2
 
