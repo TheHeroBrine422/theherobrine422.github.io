23a924d119ede67e1fcb3ec5c8dcb036262590d7
Hero <caleb@calebgj.io>
2020-01-24 13:59:31 -0600

    Delete main.js

--- CryptoGame/main.js
+++
@@ -1,5 +0,0 @@
-alert("RELOAD")
-
-algos = [["SHA256", "Bitcoin", "Bitcoin Cash"],["Scrypt", "Litecoin"],["EtHash", "Ether"],["Cryptonight", "Monero", ""]] // first in each algo, and then go on is currency
-
-function
